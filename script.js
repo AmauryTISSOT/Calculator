167c5cbdae1a4091c9af3d6047b500f17daf9bb7
amaury <tissot.amaury@gmail.com>
2022-06-11 10:19:44 +0200

    Add basics operators function in script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// function sum
+
+const sum = (a,b) => a + b;
+
+// function subtract 
+
+const subtract = (a,b) => a - b;
+
+// function multiply
+
+const multiply = (a,b) => a * b;
+
+// function divide
+
+const divide = (a,b) => a / b;
+
+// function exponentiation
+
+const exponentiation = (a,b) => a ** b;
+
+// function square root
+
+const squareRoot = (a) => Math.sqrt(a);
+
